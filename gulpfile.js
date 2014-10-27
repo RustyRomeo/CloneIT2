5ab8f35f9238347b80002dd6833dd13a2238d89f
RustyRomeo <rusty@mish.me>
2014-10-27 23:16:33 +0100

    Gulp now has a dev and a build task

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var $ = require('gulp-load-plugins')();
 
 var staticPath = './app/';
 
+// The only development task
 gulp.task('styles', function () {
     return gulp.src(staticPath + 'styles/sass/main.scss')
         .pipe($.rubySass({
@@ -26,7 +27,7 @@ gulp.task('styles', function () {
 
 // CSS concat, auto prefix, minify, then rename output file
 gulp.task('minify-css', function() {
-var cssPath = {cssSrc:['./app/styles/*.css','!*.marine-theme.css', '!*.salmon-theme.css', '!*.usa-theme.css', '!*.min.css', '!/**/*.min.css'], cssDest:'./app/styles/'};
+var cssPath = {cssSrc:['./app/styles/*.css', '!*.marine-theme.css', '!*.salmon-theme.css', '!*.usa-theme.css', '!*.min.css', '!/**/*.min.css'], cssDest:'./app/dist/styles/'};
 
   return gulp.src(cssPath.cssSrc)
     .pipe(concat('main.css'))
@@ -48,34 +49,29 @@ gulp.task('base64-css', function(){
         .pipe(gulp.dest(staticPath + '/dist/styles'));
 });
 
-gulp.task('build-js', function(){
-    return gulp.src(staticPath + 'scripts/**/*.js')
+// JS concat, strip debugging code and minify
+gulp.task('bundle-scripts', function() {
+var jsPath = {jsSrc:[
+    './app.js',
+    './app/scripts/controller/user.js',
+    './app/scripts/controller/*.js',
+    './app/scripts/*.js',
+    './app/scripts/**/*.js'],
+    jsDest:'./app/dist/scripts'};
 
-        // Removes all console.log, alerts, etc.
-        .pipe(stripDebug())
-
-        // Well, self-explanatory
-        .pipe(uglify())
-        .pipe(gulp.dest('app/dist/scripts'));
+  gulp.src(jsPath.jsSrc)
+    .pipe(concat('ngscripts.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(jsPath.jsDest));
 });
-
-
-//// JS concat, strip debugging code and minify
-//gulp.task('bundle-scripts', function() {
-//var jsPath = {jsSrc:['./app.js','.app/scripts/controller/user.js','./app/scripts/controller/*.js', './app/scripts/*.js','./app/scripts/**/*.js'], jsDest:'./app/dist/scripts'};
-//  gulp.src(jsPath.jsSrc)
-//    .pipe(concat('ngscripts.js'))
-//    .pipe(stripDebug())
-//    .pipe(uglify())
-//    .pipe(rename({ suffix: '.min' }))
-//    .pipe(gulp.dest(jsPath.jsDest));
-//});
 
 gulp.task('dev', ['styles'], function(){
     gulp.watch(staticPath + 'styles/sass/**/*.scss', ['styles']);
 });
 
-gulp.task('build', ['styles', 'minify-css', 'base64-css', 'build-js'], function(){
+gulp.task('build', ['styles', 'minify-css', 'base64-css', 'bundle-scripts'], function(){
     gulp.watch(staticPath + 'styles/sass/**/*.scss', ['styles']);
     gulp.watch(staticPath + 'scripts/**/*.js', ['build-js']);
 });
