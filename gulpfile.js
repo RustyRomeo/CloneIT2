0b867cc15bbee2e99628fb2429a60325dab5ab0d
RustyRomeo <rusty@mish.me>
2014-12-01 22:57:00 +0100

    New gulp task images that minifies the size of all contained pictures

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     autoprefix = require('gulp-autoprefixer'),
     nodemon = require('gulp-nodemon'),
     rename = require('gulp-rename'),
+    imagemin = require('gulp-imagemin'),
     livereload = require('gulp-livereload');
 
 var $ = require('gulp-load-plugins')();
@@ -16,6 +17,7 @@ var staticPath = './app/';
 /* OUR DEVELOPMENT TASK */
 
 gulp.task('styles', function () {
+
     return gulp.src(staticPath + 'styles/sass/main.scss')
         .pipe($.rubySass({
             style: 'compact',
@@ -25,16 +27,16 @@ gulp.task('styles', function () {
         .pipe(autoprefix('last 2 versions'))
         .pipe(gulp.dest('app/styles'))
         .pipe($.size());
-
 });
 
 // Runs the styles task, reloads browser and restarts server automagically after changes happened
-gulp.task('run!', ['styles'], function(){
+gulp.task('run-server', ['styles'], function(){
+
     livereload.listen();
     gulp.watch(staticPath + 'styles/sass/**/*.scss', ['styles']);
     gulp.watch(staticPath + 'styles/*.css').on('change', livereload.changed);
     gulp.watch(staticPath + 'scripts/**/*.js').on('change', livereload.changed);
-    nodemon({ script: 'server.js', ext: 'html js' })
+    nodemon({ script: 'server.js', ext: 'html js', watch: 'server/'})
     .on('restart', function () {
       console.log('restarted!')
     })
@@ -54,10 +56,19 @@ var cssPath = {cssSrc:['./app/styles/*.css', '!*.marine-theme.css', '!*.salmon-t
     .pipe(gulp.dest(cssPath.cssDest));
 });
 
-gulp.task('base64-css', function(){
-     return gulp.src(staticPath + 'styles/*.css')
+// Minify the size of all user-added pictures
+gulp.task('images', function() {
 
-        // All referenced images with maximum size of 8KB are encoded and put inline as data URIs.
+  return gulp.src(staticPath + 'images/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest(staticPath + 'dist/images'))
+    //.pipe(notify({ message: 'Images task complete' }));
+});
+
+// Encode small images with maximum size of 8KB and put inline as data URIs.
+gulp.task('base64-css', function(){
+
+     return gulp.src(staticPath + 'styles/*.css')
         .pipe(base64({
             baseDir: staticPath + 'css/',
             maxImageSize: 8 * 1024,
@@ -66,31 +77,33 @@ gulp.task('base64-css', function(){
         .pipe(gulp.dest(staticPath + '/dist/styles'));
 });
 
+// Format .scss files according to the rules defined in the .csscomb.json file
 gulp.task('css-comb', function () {
-    return gulp.src(staticPath + 'styles/sass/*.scss')
 
-         // Formats .scss files according to the rules defined in the .csscomb.json file
+    return gulp.src(staticPath + 'styles/sass/*.scss')
         .pipe(csscomb())
         .pipe(gulp.dest(staticPath + 'styles/sass'));
 });
 
 // JS concat, strip debugging code and minify
-gulp.task('bundle-scripts', function() {
-var jsPath = {
-    jsSrc:[
-    './app/scripts/app.js',
-    './app/scripts/controller/*.js',
-    './app/scripts/services/*.js',
-    './app/scripts/*.js'],
-    jsDest:'./app/dist/scripts'};
+gulp.task('bundle-scripts', function () {
+    var jsPath = {
+        jsSrc: [
+            './app/scripts/app.js',
+            './app/scripts/controller/*.js',
+            './app/scripts/services/*.js',
+            './app/scripts/*.js'],
+        jsDest: './app/dist/scripts'
+    };
 
-  gulp.src(jsPath.jsSrc)
-    .pipe(concat('scripts.js'))
-    .pipe(uglify({compress: {drop_debugger : true, drop_console: true}}))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(jsPath.jsDest));
+    gulp.src(jsPath.jsSrc)
+        .pipe(concat('scripts.js'))
+        .pipe(uglify({compress: {drop_debugger: true, drop_console: true}}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(jsPath.jsDest));
 });
 
-gulp.task('build', ['styles', 'minify-css', 'base64-css', 'bundle-scripts']);
+gulp.task('dev', ['run-server']);
+gulp.task('build', ['styles', 'minify-css', 'images', 'base64-css', 'bundle-scripts']);
 
-gulp.task('default', ['run!']);
+gulp.task('default', ['dev']);
